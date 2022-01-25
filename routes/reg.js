693125250a972922af325fa675ea48bfed8bff01
chenchao <chenchao@izuche.com>
2022-01-25 17:56:24 +0800

    fix

--- routes/reg.js
+++ routes/reg.js
@@ -4,38 +4,81 @@ var router = express.Router();
 const Db = require("../utils/Db");
 
 // 获取
-router.post('/getReg',async (req,res)=>{
-    let { name } = req['body'];
-    let regList = await Db.select(req, `SELECT * FROM regList rl WHERE rl.name  LIKE '%${name}%'`);
-    res.send({
-        code:0,
-        data:regList
-    })
+router.post('/getReg', async (req, res) => {
+    let { page, size, name } = req['body'];
+    let regList = [];
+    if (name) {
+        regList = await Db.select(req, `SELECT * FROM regList rl WHERE rl.name  LIKE '%${name}%' LIMIT ${(page - 1) * size},${size}`);
+    } else {
+        regList = await Db.select(req, `SELECT * FROM regList rl WHERE rl.name  LIMIT ${(page - 1) * size},${size}`);
+    }
+
+    let total = await Db.select(req, `select count(*) as total from regList`);
+
+    if (regList.length !== 0) {
+        res.send({
+            code: 0,
+            total:total[0].total,
+            data: regList,
+            msg: "获取成功"
+        })
+    } else {
+        res.send({
+            code: 500,
+            data: [],
+            total:total[0].total,
+            msg: "查找为空"
+        })
+    }
 })
 
 // 添加
 router.post("/addReg", async (req, res) => {
-	let { userId,articleId } = {...req['body']}
-    if (!userId || !articleId) {
+    let { name, value } = { ...req['body'] }
+    if (!name || !value) {
         res.send(MError("缺少必要条件"));
-	} else {
-        let collectId = getUUID()
-		let result = await Db.insert(req, 'collect_list', { userId,articleId,collectId });
-        await Db.update(req,'article_list','articleCollect = articleCollect + 1',` WHERE articleId = '${articleId}'`)
-		result ? res.send(Success('添加成功')) : res.send(MError('添加失败'));
-	}
+    } else {
+        let id = new Date().valueOf()
+        let result = await Db.insert(req, 'regList', { id, name, value });
+        if (result) {
+            res.send({
+                code: 0,
+                data: result,
+                msg: "添加成功"
+
+            })
+        } else {
+            res.send({
+                code: 500,
+                data: '',
+                msg: "添加失败"
+            })
+        }
+    }
 });
 
 // 删除
 router.post("/delReg", async (req, res) => {
-	let { articleId,userId } = {...req['body']}
-    if (!userId || !articleId) {
+    let { id } = { ...req['body'] }
+    if (!id) {
         res.send(MError("缺少必要条件"));
-	} else {
-		let result = await Db.delete(req, `DELETE FROM collect_list  WHERE userId = '${userId}' AND articleId = '${articleId}'`);
-        await Db.update(req,'article_list','articleCollect = articleCollect - 1',` WHERE articleId = '${articleId}'`)
-        result ? res.send(Success('取消收藏成功')) : res.send(MError('取消收藏失败'));
-	}
+    } else {
+        let result = await Db.delete(req, `DELETE FROM regList  WHERE id = '${id}'`);
+        if (result) {
+            res.send({
+                code: 0,
+                data: result,
+                msg: "删除成功"
+
+            })
+        } else {
+            res.send({
+                code: 500,
+                data: '',
+                msg: "删除失败"
+            })
+        }
+    }
 });
 
 module.exports = router;
