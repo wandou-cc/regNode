00e01426eb37d984a2f3aa9f7f0487e06b5228c1
chenchao <chenchao@izuche.com>
2022-01-26 16:17:01 +0800

    fixfix

--- routes/reg.js
+++ routes/reg.js
@@ -7,18 +7,18 @@ const Db = require("../utils/Db");
 router.post('/getReg', async (req, res) => {
     let { page, size, name } = req['body'];
     let regList = [];
+    let total = "";
     if (name) {
-        regList = await Db.select(req, `SELECT * FROM regList rl WHERE rl.name  LIKE '%${name}%' LIMIT ${(page - 1) * size},${size}`);
+        regList = await Db.select(req, `SELECT * FROM regList rl WHERE rl.name = ${name}  LIKE '%${name}%' LIMIT ${(page - 1) * size},${size}`);
+        total = await Db.select(req, `select count(*) as total from regList  WHERE regList.name  LIKE '%${name}%'`);
     } else {
-        regList = await Db.select(req, `SELECT * FROM regList rl WHERE rl.name  LIMIT ${(page - 1) * size},${size}`);
+        regList = await Db.select(req, `SELECT * FROM regList  LIMIT ${(page - 1) * size},${size}`);
+        total = await Db.select(req, `select count(*) as total from regList`);
     }
-
-    let total = await Db.select(req, `select count(*) as total from regList`);
-
     if (regList.length !== 0) {
         res.send({
             code: 0,
-            total:total[0].total,
+            total: total[0].total,
             data: regList,
             msg: "获取成功"
         })
@@ -26,59 +26,11 @@ router.post('/getReg', async (req, res) => {
         res.send({
             code: 500,
             data: [],
-            total:total[0].total,
+            total: total[0].total,
             msg: "查找为空"
         })
     }
 })
 
-// 添加
-router.post("/addReg", async (req, res) => {
-    let { name, value } = { ...req['body'] }
-    if (!name || !value) {
-        res.send(MError("缺少必要条件"));
-    } else {
-        let id = new Date().valueOf()
-        let result = await Db.insert(req, 'regList', { id, name, value });
-        if (result) {
-            res.send({
-                code: 0,
-                data: result,
-                msg: "添加成功"
-
-            })
-        } else {
-            res.send({
-                code: 500,
-                data: '',
-                msg: "添加失败"
-            })
-        }
-    }
-});
-
-// 删除
-router.post("/delReg", async (req, res) => {
-    let { id } = { ...req['body'] }
-    if (!id) {
-        res.send(MError("缺少必要条件"));
-    } else {
-        let result = await Db.delete(req, `DELETE FROM regList  WHERE id = '${id}'`);
-        if (result) {
-            res.send({
-                code: 0,
-                data: result,
-                msg: "删除成功"
-
-            })
-        } else {
-            res.send({
-                code: 500,
-                data: '',
-                msg: "删除失败"
-            })
-        }
-    }
-});
 
 module.exports = router;
